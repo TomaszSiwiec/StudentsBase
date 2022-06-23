4895c8d49d2a62cc3b3a4d39b1566871e4669a97
Tomasz Siwiec <ts@onet.pl>
2022-06-23 23:02:57 +0200

    Second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import {getAllStudents} from "./client";
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+    LoadingOutlined,
+} from '@ant-design/icons';
+import {
+    Breadcrumb, Empty,
+    Layout,
+    Menu, Spin,
+    Table
+} from 'antd';
+
+const {Header, Content, Footer, Sider} = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined/>),
+    getItem('Option 2', '2', <DesktopOutlined/>),
+    getItem('User', 'sub1', <UserOutlined/>, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined/>),
+];
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
+
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const [fetching, setFetching] = useState(true);
+
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+                setFetching(false);
+            });
+
+    useEffect(() => {
+        console.log("components is mounted");
+        fetchStudents();
+    }, []);
+
+    const renderStudents = () => {
+        if (fetching) {
+            return <Spin indicator={antIcon}/>
+        }
+
+        if (students.length <= 0) {
+            return <Empty />
+        }
+
+        return <Table
+            dataSource={students}
+            columns={columns}
+            bordered
+            title={() => "Students"}
+            pagination={{pageSize: 50}}
+            scroll={{y: 240}}
+            rowKey={(student) => student.id}
+        />;
+    };
+
+    return <Layout style={{minHeight: '100vh'}}>
+        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+            <div className="logo"/>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+        </Sider>
+        <Layout className="site-layout">
+            <Header className="site-layout-background" style={{padding: 0}}/>
+            <Content style={{margin: '0 16px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                    {renderStudents()}
+                </div>
+            </Content>
+            <Footer style={{textAlign: 'center'}}>Tomasz Siwiec ©2022</Footer>
+        </Layout>
+    </Layout>
 }
 
 export default App;
